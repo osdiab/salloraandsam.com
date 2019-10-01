3db83fcdb2d1f857341577ab7fb58c8402fb6af9
Omar Diab <od@eco.com>
2019-10-01 16:29:39 +0900

    sallora and sam header

--- src/@app/presentation/pages/DefaultLayout/Header.tsx
+++ src/@app/presentation/pages/DefaultLayout/Header.tsx
@@ -1,43 +1,40 @@
 import * as React from "react";
 import styled from "styled-components/macro";
 
-import avatarImg from "@app/assets/avatar.jpg";
 import { PageSection } from "@app/presentation/utility/mixins/PageSection";
+import { normalWeightMixin } from "@app/presentation/theme/text";
 
 const HeaderElem = styled.header`
-  ${PageSection} display: flex;
-  flex-direction: row;
-  align-items: center;
+  ${PageSection}
   margin-bottom: 0;
   border: 0;
 `;
-const Avatar = styled.span`
-  max-width: 140px;
-  min-height: 140px;
-  flex: 1;
-  align-self: stretch;
-
-  background-image: url(${avatarImg});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-
-  margin-right: 20px;
+const HeaderRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
-const BannerTitle = styled.h1`
-  margin: 0;
-  flex: 1;
-`;
-const HighlightName = styled.span`
+const HeaderTitle = styled.h1`
   color: ${props => props.theme.palette.primary};
-  display: inline-block;
+  text-align: center;
+  font-size: 2.5rem;
+  ${normalWeightMixin}
+`;
+
+const HeaderSubtitle = styled.h2`
+  text-align: center;
+  font-size: 1.5rem;
+  ${normalWeightMixin};
 `;
 
 export const Header: React.StatelessComponent<{}> = () => (
   <HeaderElem>
-    <Avatar />
-    <BannerTitle>
-      I am <HighlightName>Omar Diab.</HighlightName>
-    </BannerTitle>
+    <HeaderRow>
+      <HeaderTitle>Sallora & Sam</HeaderTitle>
+    </HeaderRow>
+    <HeaderRow>
+      <HeaderSubtitle>pastry and catering</HeaderSubtitle>
+    </HeaderRow>
   </HeaderElem>
 );
