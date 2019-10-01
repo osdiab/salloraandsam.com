8a6f9098845979ebe74a26a91ff77cdcf890d971
Omar Diab <od@eco.com>
2019-10-01 16:44:55 +0900

    expose spacing module

--- src/@app/presentation/theme/index.ts
+++ src/@app/presentation/theme/index.ts
@@ -1,10 +1,12 @@
 import { mediaQueries } from "@app/presentation/theme/mediaQueries";
 import { palette } from "@app/presentation/theme/palette";
 import { text } from "@app/presentation/theme/text";
+import { spacingInRem } from "@app/presentation/theme/spacing";
 
 export const theme = {
   palette,
   text,
+  spacingInRem,
   mediaQueries
 };
 
