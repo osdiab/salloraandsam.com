48fda526b63ac82ab6077c30117ee58625b05e37
Omar Diab <od@eco.com>
2019-10-01 16:29:17 +0900

    ditch pagebody component

--- src/@app/presentation/utility/layout/PageBody.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components/macro";
-
-import { PageSection } from "@app/presentation/utility/mixins/PageSection";
-
-export const PageBody = styled.main`
-  ${PageSection};
-`;
