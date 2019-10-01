39ec68554dccd1969c90637229ffecb42f90b46a
Omar Diab <od@eco.com>
2019-10-01 16:44:38 +0900

    base design for banner

--- src/@app/presentation/pages/HomePage/index.tsx
+++ src/@app/presentation/pages/HomePage/index.tsx
@@ -1,25 +1,62 @@
 import * as React from "react";
 import styled from "styled-components/macro";
 
-import { PageBody } from "@app/presentation/utility/layout/PageBody";
+import {
+  Button,
+  ButtonRole,
+  ButtonTargetKind
+} from "@app/presentation/utility/Button";
+import { normalWeightMixin } from "@app/presentation/theme/text";
 
-const BannerTitle = styled.p`
+const BannerSection = styled.section`
+  background-color: ${props => props.theme.palette.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: ${props => props.theme.spacingInRem.xxxl};
+  padding-bottom: ${props => props.theme.spacingInRem.xxxl};
+`;
+
+const BannerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+const BannerTitle = styled.h1`
   font-size: 2rem;
-  font-weight: 700;
-  max-width: 80%;
-  margin: auto;
-  text-align: center;
+  color: ${props => props.theme.palette.whiteText};
+  ${normalWeightMixin};
+`;
+const BannerSubtitle = styled.h2`
+  font-size: 1.3rem;
+  color: ${props => props.theme.palette.whiteText};
+`;
+const Placeholder = styled.div`
+  width: 300px;
+  height: 300px;
 `;
 export class HomePage extends React.Component<{}> {
   public render() {
     return (
       <>
-        <PageBody>
-          <BannerTitle>
-            I design and implement simple, powerful and easily maintainable
-            products from start to finish.
-          </BannerTitle>
-        </PageBody>
+        <main>
+          <BannerSection>
+            <Placeholder />
+            <BannerText>
+              <BannerTitle>Fine Syrian pastries since 1873</BannerTitle>
+              <BannerSubtitle>
+                Order catering for your next gathering
+              </BannerSubtitle>
+              <Button
+                role={ButtonRole.PRIMARY_INVERTED}
+                onClick={{ kind: ButtonTargetKind.LINK, action: "#contact" }}
+              >
+                Get in touch
+              </Button>
+            </BannerText>
+          </BannerSection>
+        </main>
       </>
     );
   }
