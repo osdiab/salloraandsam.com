92a3ff61a36e6e15dc56b8e77c6d075633dee803
Omar Diab <od@eco.com>
2019-10-12 23:12:59 +0900

    add gray to palette

--- src/@app/presentation/theme/palette.ts
+++ src/@app/presentation/theme/palette.ts
@@ -1,5 +1,9 @@
+/**
+ * Actual colors used in app
+ */
 export const colors = {
   offWhite: "#f6f6f6",
+  lightGray: "#dadada",
   mediumGray: "#888888",
   offBlack: "#2a2a2a",
 
@@ -8,10 +12,14 @@ export const colors = {
   paleHotPink: "#f25a99"
 };
 
+/**
+ * Colors used in app tagged by their primary use
+ */
 export const palette = {
   primary: colors.hotPink,
   primaryHighlight: colors.paleHotPink,
   background: colors.offWhite,
+  grayBackground: colors.lightGray,
   disabled: colors.mediumGray,
   bodyText: colors.offBlack,
   whiteText: colors.offWhite
