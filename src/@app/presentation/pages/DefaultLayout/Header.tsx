9349ef29d89886f7d852e0eea3069e0f5194abdd
Omar Diab <od@eco.com>
2019-10-12 21:59:32 +0900

    add new header layout

--- src/@app/presentation/pages/DefaultLayout/Header.tsx
+++ src/@app/presentation/pages/DefaultLayout/Header.tsx
@@ -2,54 +2,62 @@ import * as React from "react";
 import styled from "styled-components/macro";
 
 import { PageSection } from "@app/presentation/utility/mixins/PageSection";
-import { normalWeightMixin } from "@app/presentation/theme/text";
-import { TextLogo as SvgTextLogo } from "@app/presentation/svg/TextLogo";
+import { TextLogoArabic as SvgTextLogoArabic } from "@app/presentation/svg/TextLogoArabic";
+import { TextLogoEnglish as SvgTextLogoEnglish } from "@app/presentation/svg/TextLogoEnglish";
+import { HeaderSubtitle as SvgHeaderSubtitle } from "@app/presentation/svg/HeaderSubtitle";
+import { SweetsShopArabic as SvgSweetsShopArabic } from "@app/presentation/svg/SweetsShopArabic";
 
 const HeaderElem = styled.header`
   ${PageSection}
   margin-bottom: 0;
   border: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   > *:not(:last-child) {
-    margin-bottom: ${props => props.theme.spacingInRem.xs};
+    margin-bottom: ${props => props.theme.spacingInRem.m};
   }
 `;
-const HeaderRow = styled.div`
+
+const TextLogoEnglish = styled(SvgTextLogoEnglish)`
+  height: 1.8rem;
+  path {
+    fill: ${props => props.theme.palette.primary};
+  }
+`;
+
+const ArabicLogo = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-const HeaderTitle = styled.h1`
-  color: ${props => props.theme.palette.primary};
-  text-align: center;
-  font-size: 2.5rem;
-  ${normalWeightMixin}
+  flex-direction: column;
+  align-items: flex-end;
 `;
 
-const HeaderSubtitle = styled.h2`
-  text-align: center;
-  font-size: 1.5rem;
-  ${normalWeightMixin};
-`;
-
-const TextLogo = styled(SvgTextLogo)`
+const TextLogoArabic = styled(SvgTextLogoArabic)`
   height: 2.2rem;
   path {
     fill: ${props => props.theme.palette.primary};
   }
 `;
 
+const SweetsShopArabic = styled(SvgSweetsShopArabic)`
+  height: 1rem;
+  path {
+    fill: ${props => props.theme.palette.bodyText};
+  }
+  margin-bottom: -0.5rem; /* tuning spacing */
+`;
+const HeaderSubtitle = styled(SvgHeaderSubtitle)`
+  height: 1.4rem;
+`;
+
 export const Header: React.FC = () => (
   <HeaderElem>
-    <HeaderRow>
-      <HeaderTitle>Sallora & Sam</HeaderTitle>
-    </HeaderRow>
-    <HeaderRow>
-      <TextLogo />
-    </HeaderRow>
-    <HeaderRow>
-      <HeaderSubtitle>pastry and catering</HeaderSubtitle>
-    </HeaderRow>
+    <TextLogoEnglish />
+    <ArabicLogo>
+      <TextLogoArabic />
+      <SweetsShopArabic />
+    </ArabicLogo>
+    <HeaderSubtitle />
   </HeaderElem>
 );
