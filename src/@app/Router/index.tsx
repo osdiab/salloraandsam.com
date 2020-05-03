8a096d532555b7b3c3cfe11882afad2a46082546
Omar Diab <me@omardiab.com>
2020-05-03 10:42:22 +0900

    add redireciton logic

--- src/@app/Router/index.tsx
+++ src/@app/Router/index.tsx
@@ -1,16 +1,35 @@
 import * as React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  useLocation
+} from "react-router-dom";
+import isRelativeUrl from "is-relative-url";
 
 import { HomePage } from "@app/presentation/pages/HomePage";
 import { NotFound } from "@app/presentation/pages/NotFound";
 import { DefaultLayoutRoute } from "@app/Router/DefaultLayoutRoute";
 import { MenuPage } from "@app/presentation/pages/Menu";
 
+const Redirector = () => {
+  const location = useLocation();
+  const path = new URLSearchParams(location.search.substr(1)).get("to");
+  if (!path || !isRelativeUrl(path)) {
+    return <Redirect to="/" />;
+  }
+  return <Redirect to={path} />;
+};
+
 export const Router: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <DefaultLayoutRoute exact={true} path="/" component={HomePage} />
       <DefaultLayoutRoute exact={true} path="/menu" component={MenuPage} />
+      <Route exact={true} path="/r">
+        <Redirector />
+      </Route>
       <DefaultLayoutRoute component={NotFound} />
     </Switch>
   </BrowserRouter>
--- src/@app/presentation/pages/NotFound/index.tsx
+++ src/@app/presentation/pages/NotFound/index.tsx
@@ -1,9 +1,41 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export const NotFound: React.FC = () => (
-  <main>
-    <h1>Not Found</h1>
-    <Link to="/">Back</Link>
-  </main>
-);
+export const NotFound: React.FC = () => {
+  const location = useLocation();
+  const [shouldRefresh, setShouldRefresh] = React.useState<boolean>();
+
+  React.useEffect(() => {
+    let subscribed = true;
+    if (shouldRefresh !== undefined) {
+      return;
+    }
+    if (!location.pathname.endsWith(".pdf")) {
+      setShouldRefresh(true);
+      return;
+    }
+    fetch(location.pathname).then(res => {
+      if (!subscribed) {
+        return;
+      }
+      if (res.status < 400) {
+        window.location.reload();
+      }
+      setShouldRefresh(true);
+    });
+    return () => {
+      subscribed = false;
+    };
+  }, [shouldRefresh, location]);
+
+  if (shouldRefresh === undefined || shouldRefresh) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <main>
+      <h1>Not Found</h1>
+      <Link to="/">Back</Link>
+    </main>
+  );
+};
